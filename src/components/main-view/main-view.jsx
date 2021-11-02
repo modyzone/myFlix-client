2141a7bbe1afc78c60dbdd81d646222614e012ba
Modyzone <mody.zone@outlook.com>
2021-11-02 20:46:24 +0100

    add new files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { MovieCard } from '../movie-card/movie-card';
 export class MainView extends React.Component {
     constructor() {
         super();
@@ -12,18 +13,19 @@ export class MainView extends React.Component {
         }
     }
 
-render() {
-    return (
-        <div className="main-view">
-            <div>Inception</div>
-            <div>The Shawshank Redemption</div>
-            <div>Gladiator</div>
-        </div>
-    );
-       }
+    render() {
+        const { movies } = this.state;
+
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+        return (
+            <div className="main-view">
+                {movies.map(movie => <MovieCard key={movie._id} movieData={movie}/>)}
+            </div>
+        );
+    }
 }
 export default MainView;
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import '../../index.scss';
-   
